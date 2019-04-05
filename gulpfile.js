43df7aabe6c7a25713abd48c28d48424c96ced44
Ryan Palma <ryahpalma@gmail.com>
2019-04-05 00:59:18 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,14 @@ gulp.task('server', ['sass'], function() {
     browserSync.init({
         server: "."
     });
-// If you need to change your directory, change the values below "new_directory/*scss"
+// If you need to change your directory, change the values below "new_directory/*.scss"
     gulp.watch("scss/*.scss", ['sass']); //Watcher folder directory and file extension 
     gulp.watch("*.html").on('change', browserSync.reload); // Watcher of BrowserSync
 });
 
 // Compile SASS into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-// If you need to change your directory, change the values below "new_directory/*scss"
+// If you need to change your directory, change the values below "new_directory/*.scss"
     return gulp.src('scss/*.scss') // Folder directory and file extension
         .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError)) // In OutputStyle select if you want compact,expanded,nested or compressed
         .pipe(gulp.dest('assets/css')) // Select the directory for the CSS file
