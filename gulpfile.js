0bc7e44fc2bb24a9e05f828d9d6f5f5b3c00f733
Ryan Palma <ryahpalma@gmail.com>
2019-04-19 22:42:24 -0300

    Code updated for gulp 4.0.0
    Now the CSS file is automatically renamed with .min

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,40 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-
-// Static Server + Watching SCSS/HTML files
-gulp.task('server', ['sass'], function() {
-
-    browserSync.init({
-        server: "."
-    });
+// Load Plugins
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const browsersync = require('browser-sync').create();
+// Static Server
+function browserSync() {
+        browsersync.init({
+          server: {
+            baseDir: "."
+          }
+        });
+}
+// BrowserSync Reload
+function browserSyncReload() {
+    browsersync.reload();
+}
+// Watch Files
+function watchFiles() {
 // If you need to change your directory, change the values below "new_directory/*.scss"
-    gulp.watch("scss/*.scss", ['sass']); //Watcher folder directory and file extension 
-    gulp.watch("*.html").on('change', browserSync.reload); // Watcher of BrowserSync
-});
-
-// Compile SASS into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+    gulp.watch("scss/*.scss", scss); // Watch and build CSS file
+    gulp.watch("*.html").on('change', browserSyncReload); // Reload when HTML is changed
+}
+// Compile SCSS into CSS & auto-inject into browsers
+function scss() {
 // If you need to change your directory, change the values below "new_directory/*.scss"
     return gulp.src('scss/*.scss') // Folder directory and file extension
         .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError)) // In OutputStyle select if you want compact,expanded,nested or compressed
+        .pipe(rename({suffix:'.min'})) // Rename compressed CSS
         .pipe(gulp.dest('assets/css')) // Select the directory for the CSS file "new_directory/css"
-        .pipe(browserSync.stream());
-});
-
-gulp.task('default', ['server']); // Initialize all tasks
+        .pipe(browsersync.stream());
+}
+// Define Complex Tasks
+const build = gulp.series(gulp.parallel(scss));
+const start = gulp.parallel(watchFiles, browserSync);
+//Export Tasks
+exports.scss = scss;
+exports.start = start;
+exports.build = build; 
+exports.default = start; // Initialize all tasks
